ebc99cc08be2607b9b347b581682603ed723e64d
Felipe Eduardo Calderon Espinoza <felipe.calderon321@gmail.com>
2025-05-26 17:25:53 -0400

    prettier global

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  return (
-    <div className="">
-      <p>Hola, mundo</p>
-      <Button>Botón de shadcn de prueba</Button>
-    </div>
-  );
+    return (
+        <div className="">
+            <p>Hola, mundo</p>
+            <Button>Botón de shadcn de prueba</Button>
+        </div>
+    )
 }
