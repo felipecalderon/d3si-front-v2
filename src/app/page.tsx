e1f1457bd256eadc60b2210dd8f24a0c52952a6c
Felipe Eduardo Calderon Espinoza <felipe.calderon321@gmail.com>
2025-05-26 17:16:10 -0400

    shadcn configuración basica

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="">
       <p>Hola, mundo</p>
+      <Button>Botón de shadcn de prueba</Button>
     </div>
   );
 }
