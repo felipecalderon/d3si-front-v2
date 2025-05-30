14ea33e71ef01bf884efd32697576c52896f1cd2
Felipe Eduardo Calderon Espinoza <felipe.calderon321@gmail.com>
2025-05-30 11:36:21 -0400

    revisando detallitos

--- src/actions/productsActions/getAllProducts.ts
+++ src/actions/productsActions/getAllProducts.ts
@@ -1,5 +1,5 @@
-// URL base de la API, definida en variables de entorno
-const API_URL = process.env.NEXT_PUBLIC_API_URL
+import { API_URL } from "@/lib/enviroments"
+import { fetcher } from "@/lib/fetcher"
 
 /**
  * Obtiene todos los productos desde la API.
@@ -9,7 +9,6 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL
  */
 
 export const getAllProducts = async () => {
-    const response = await fetch(`${API_URL}/products`)
-    const products = await response.json()
+    const products = await fetcher(`${API_URL}/products`)
     return products
 }
