31537f798ac7f8808939b82be12dfcfa82ad8cd7
C4nd3l-m <c4nd3l.m@gmail.com>
2025-05-31 20:27:44 -0300

    products actions

---
+++ src/actions/products/updateProducts.ts
@@ -0,0 +1,10 @@
+/*const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+export const updateProduct = async (product) =>{
+    try{
+        const response = await fetch(`${API_URL}/products/?${queryParams.toString()}`)
+    }
+    catch(error){
+
+    }
+}*/
