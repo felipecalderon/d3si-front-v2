9cef6f2a354c7fcf029d969097f2eb9ff90dbcd6
C4nd3l-m <c4nd3l.m@gmail.com>
2025-05-31 23:18:36 -0300

    Improving documentation

--- src/actions/products/getAllProducts.ts
+++ src/actions/products/getAllProducts.ts
@@ -1,14 +1,18 @@
+import { IProduct } from "@/interfaces/IProduct"
 import { API_URL } from "@/lib/enviroments"
 import { fetcher } from "@/lib/fetcher"
 
 /**
  * Obtiene todos los productos desde la API.
- * Realiza una petición GET a la ruta `/products` y devuelve los datos en formato JSON.
+ * Realiza una petición GET a la ruta `/products` y devuelve los datos como un arreglo de productos.
+ *
+ * @returns {Promise<IProduct[]>} - Promesa que resuelve con un array de objetos `IProduct`.
  * 😊
- * TODO: Agregar tipado a la función y al valor retornado.
+ * @example
+ * const products = await getAllProducts();
  */
 
-export const getAllProducts = async () => {
-    const products = await fetcher(`${API_URL}/products`)
+export const getAllProducts = async (): Promise<IProduct[]> => {
+    const products: IProduct[] = await fetcher(`${API_URL}/products`)
     return products
 }
--- src/actions/products/updateProducts.ts
+++ src/actions/products/updateProducts.ts
@@ -2,12 +2,39 @@ import { fetcher } from "@/lib/fetcher"
 import { API_URL } from "@/lib/enviroments"
 import { IProduct } from "@/interfaces/IProduct"
 
+/**
+ * Actualiza un producto en la base de datos mediante una petición HTTP PUT.
+ * Utiliza la función `fetcher` para manejar la solicitud.
+ *
+ * @param {IProduct} product - Objeto del producto que se desea actualizar.
+ * Debe incluir el `productID` que será usado para identificar el recurso en la URL.
+ *
+ * @returns {Promise<void>} - No devuelve nada si la operación es exitosa.
+ *
+ * @throws {Error} - En caso de error, se muestra un mensaje en la consola.
+ *
+ * @example
+ * await updateProduct({
+ *   productID: "123",
+ *   name: "Zapatilla",
+ *   image: "https://...",
+ *   totalProducts: 100,
+ *   createdAt: "...",
+ *   updatedAt: "...",
+ *   ProductVariations: [...]
+ * });
+ */
 
-export const updateProduct = async (product: IProduct): Promise<void> =>{
-    try{
-        const response = await fetcher(`${API_URL}/products/?${queryParams.toString()}`)
-    }
-    catch(error){
-
+export const updateProduct = async (product: IProduct): Promise<void> => {
+    try {
+        await fetcher<void>(`${API_URL}/products/${product.productID}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(product),
+        })
+    } catch (error) {
+        console.error("Error updating product", error)
     }
 }
