8db349abeb411fbba3bf3e3facb2caaac9a88cd9
Katherine Contreras <kathijcs@gmail.com>
2025-06-05 11:01:29 -0300

    added logout to sidebar

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -1,11 +1,13 @@
-import { create } from "zustand"
+import { create } from 'zustand'
 
 interface UserStore {
     user: any
     setUser: (user: any) => void
+    logout: () => void
 }
 
 export const useAuth = create<UserStore>((set) => ({
     user: null,
     setUser: (user) => set({ user }),
-}))
+    logout: () => set({ user: null }),
+}));
