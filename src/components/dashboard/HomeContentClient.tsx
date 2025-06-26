520af528a91bff6509babaa36e18cbc74be45533
C4nd3l-m <c4nd3l.m@gmail.com>
2025-06-26 20:53:44 -0300

    resumen de ventas conectado al back

--- src/components/dashboard/HomeContentClient.tsx
+++ src/components/dashboard/HomeContentClient.tsx
@@ -6,6 +6,7 @@ import SalesTable from "@/components/ListTable/SalesTable"
 import Link from "next/link"
 import { ArrowRightLeft, CreditCard, HandCoins, Wallet, FileText, FileCheck2, DollarSign } from "lucide-react"
 import { ISaleResponse } from "@/interfaces/sales/ISale"
+import { IResume } from "@/interfaces/sales/ISalesResume"
 
 const GaugeChart = dynamic(() => import("@/components/dashboard/GaugeChart"), {
     ssr: false,
@@ -13,16 +14,29 @@ const GaugeChart = dynamic(() => import("@/components/dashboard/GaugeChart"), {
 
 interface Props {
     sales: ISaleResponse[]
+    resume: IResume
 }
 
-export default function HomeContentClient({ sales }: Props) {
+export default function HomeContentClient({ sales, resume }: Props) {
     return (
         <>
             <div className="grid grid-cols-3 gap-6 items-start">
                 <div className="flex flex-col gap-4">
-                    <StatCard icon={<FileText />} label="Boletas Emitidas" value="128" />
-                    <StatCard icon={<FileCheck2 />} label="Facturas Emitidas" value="31" />
-                    <StatCard icon={<DollarSign />} label="Facturación" value="$45.846.410" />
+                    <StatCard
+                        icon={<FileText />}
+                        label="Boletas Emitidas"
+                        value={resume.orders.month.count.toString()}
+                    />
+                    <StatCard
+                        icon={<FileCheck2 />}
+                        label="Facturas Emitidas"
+                        value={resume.sales.month.count.toString()}
+                    />
+                    <StatCard
+                        icon={<DollarSign />}
+                        label="Facturación"
+                        value={`$${resume.sales.month.amount.toLocaleString("es-CL")}`}
+                    />
                 </div>
 
                 <div className="flex justify-center items-center h-full">
@@ -32,12 +46,22 @@ export default function HomeContentClient({ sales }: Props) {
                 </div>
 
                 <div className="flex flex-col gap-4">
-                    <StatCard icon={<DollarSign />} label="Ventas del día" value="$435.670" color="text-green-600" />
-                    <StatCard icon={<DollarSign />} label="Ventas de ayer" value="$635.800" color="text-yellow-600" />
+                    <StatCard
+                        icon={<DollarSign />}
+                        label="Ventas del día"
+                        value={`$${resume.sales.today.amount.toLocaleString("es-CL")}`}
+                        color="text-green-600"
+                    />
+                    <StatCard
+                        icon={<DollarSign />}
+                        label="Ventas de ayer"
+                        value={`$${resume.sales.yesterday.amount.toLocaleString("es-CL")}`}
+                        color="text-yellow-600"
+                    />
                     <StatCard
                         icon={<DollarSign />}
                         label="Ventas Semana móvil"
-                        value="$3.535.800"
+                        value={`$${resume.sales.last7.amount.toLocaleString("es-CL")}`}
                         color="text-red-600"
                     />
                 </div>
