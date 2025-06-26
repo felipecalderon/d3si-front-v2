e0ee337a254b09b0af314c115aab5cc1713f7fe5
Felipe Eduardo Calderon Espinoza <felipe.calderon321@gmail.com>
2025-06-26 12:21:40 -0400

    rename home

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -3,8 +3,8 @@ import { getSales } from "@/actions/sales/getSales"
 import HomeContentClient from "@/components/dashboard/HomeContentClient"
 
 const HomePage = async () => {
-    const storeID = "f3c9d8e0-ccaf-4300-a416-c3591c4d8b52" // ID hardcodeado por ahora
-    const sales = (await getSales(storeID)) as Sale[]
+    const storeID = "f3c9d8e0-ccaf-4300-a416-c3591c4d8b52"
+    const sales = await getSales(storeID)
 
     return <HomeContentClient sales={sales} />
 }
