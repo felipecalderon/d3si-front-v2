c8f7d1681ced4d5b5b19609d0a3b001fcbb32850
mikeagf300 <mikeagf300@gmail.com>
2025-06-26 09:17:17 -0600

    Se corrige redireccion de crear OC en sidebar y se añade label Facturas

--- src/utils/navItems.ts
+++ src/utils/navItems.ts
@@ -1,45 +1,46 @@
-import { 
-  FaHome, 
-  FaBox, 
-  FaFileInvoice, 
-  FaCalculator, 
-  FaPlusCircle, 
-  FaUsers, 
-  FaChartLine, 
-  FaChartBar 
+import {
+    FaHome,
+    FaBox,
+    FaFileInvoice,
+    FaCalculator,
+    FaPlusCircle,
+    FaUsers,
+    FaChartLine,
+    FaChartBar,
 } from "react-icons/fa"
 
 export const navItems = [
-  { 
-    label: "Caja", 
-    route: "/home", 
-    icon: FaHome 
-  },
-  { 
-    label: "Inventario", 
-    route: "/home/inventory", 
-    icon: FaBox 
-  },
-  { 
-    label: "Facturación", 
-    icon: FaFileInvoice,
-    subItems: [
-      { label: "Crear OC", route: "/home/facturacion/crear-oc" },
-      { label: "Cotizar", route: "/home/facturacion/cotizar" }
-    ]
-  },
-  { 
-    label: "UTI", 
-    route: "/home/usuarios", 
-    icon: FaUsers 
-  },
-  { 
-    label: "Control de Mando", 
-    route: "/home/controlDeMando", 
-    icon: FaChartLine 
-  },
-  { 
-    label: "Estado de Resultados", 
-    icon: FaChartBar 
-  }
+    {
+        label: "Caja",
+        route: "/home",
+        icon: FaHome,
+    },
+    {
+        label: "Inventario",
+        route: "/home/inventory",
+        icon: FaBox,
+    },
+    {
+        label: "Facturación",
+        icon: FaFileInvoice,
+        subItems: [
+            { label: "Crear OC", route: "/home/purchaseOrder" },
+            { label: "Facturas", route: "/home/invoices" },
+            { label: "Cotizar", route: "/home/facturacion/cotizar" },
+        ],
+    },
+    {
+        label: "UTI",
+        route: "/home/usuarios",
+        icon: FaUsers,
+    },
+    {
+        label: "Control de Mando",
+        route: "/home/controlDeMando",
+        icon: FaChartLine,
+    },
+    {
+        label: "Estado de Resultados",
+        icon: FaChartBar,
+    },
 ]
