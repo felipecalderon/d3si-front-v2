2e5edb912a04942c52951388b989d5c0c84a10f8
mikeagf300 <mikeagf300@gmail.com>
2025-07-15 13:54:19 -0600

    Se implementa persistencia del usuario en local storage con zustand

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -1,21 +1,29 @@
 import { create } from "zustand"
+import { persist } from "zustand/middleware"
 import { IUser } from "@/interfaces/users/IUser"
-import { IStore } from "@/interfaces/stores/IStore"
+//import { IStore } from "@/interfaces/stores/IStore"
 import { useTienda } from "./tienda.store"
 
 interface UserStore {
-    user: any
-    setUser: (user: any) => void
+    user: IUser | null
+    setUser: (user: IUser) => void
     logout: () => void
 }
 
-export const useAuth = create<UserStore>((set) => ({
-    user: null,
-    setUser: (user) => set({ user }),
-    logout: () => {
-        const { setStores, setUsers } = useTienda.getState()
-        setStores([])
-        setUsers([])
-        set({ user: null })
-    },
-}))
+export const useAuth = create<UserStore>()(
+    persist(
+        (set) => ({
+            user: null,
+            setUser: (user) => set({ user }),
+            logout: () => {
+                const { setStores, setUsers } = useTienda.getState()
+                setStores([])
+                setUsers([])
+                set({ user: null })
+            },
+        }),
+        {
+            name: "auth-storage", // nombre de la clave en localStorage
+        }
+    )
+)
