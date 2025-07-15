287b1efdb3835fb921ebb77bc86892bbd291c76c
mikeagf300 <mikeagf300@gmail.com>
2025-07-15 15:31:08 -0600

    Se ocultan vistas al vendedor asi como funcionalidades de la aplicacion, aun se esta trabajando con mas funcionalidades que no debe tener habilitadas

--- src/components/Inventory/InventoryActions.tsx
+++ src/components/Inventory/InventoryActions.tsx
@@ -4,25 +4,24 @@ import { useRouter } from "next/navigation"
 import { IProduct } from "@/interfaces/products/IProduct"
 import { exportInventoryToExcel } from "@/utils/exportInventoryToExcel"
 import { Button } from "../ui/button"
-import { useState } from "react"
 
 export default function InventoryActions({ products }: { products: IProduct[] }) {
     const router = useRouter()
     const handleDownloadExcel = () => {
         if (!products || products.length === 0) {
-        alert("No hay productos para exportar.")
-        return
+            alert("No hay productos para exportar.")
+            return
         }
         exportInventoryToExcel(products)
     }
 
     return (
         <div className="flex gap-2">
-            <Button 
-                onClick={() => router.push("/home/inventory/create")} 
+            <Button
+                onClick={() => router.push("/home/inventory/create")}
                 className="bg-green-600 text-white px-4 py-2 rounded"
-                >
-            Crear Producto
+            >
+                Crear Producto
             </Button>
             <Button onClick={handleDownloadExcel} className="bg-blue-600 text-white px-4 py-2 rounded">
                 Descargar Excel
