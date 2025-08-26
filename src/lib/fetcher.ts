0348554d3f621d6a5dc36887b822cbb05b6463ed
Felipe <felipe.calderon321@gmail.com>
2025-08-25 22:47:09 -0400

    rebase fetcher

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -13,7 +13,7 @@
  */
 
 export const fetcher = async <T>(url: string, options: RequestInit = {}): Promise<T> => {
-    const response = await fetch(url, { ...options, cache: "no-store" })
+    const response = await fetch(url, options)
 
     const data = await response.json()
     return data
